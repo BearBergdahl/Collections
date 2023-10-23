109b3638ec7fbf75840992c7f4f5dc953cc061f6
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-10-23 14:51:54 +0200

    Add project files.

---
+++ Collections/DemoClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal static class DemoClass
+    {
+        static void DemoArrayList()
+        {
+            ArrayList simpleList = new ArrayList();
+        }
+
+
+    }
+}
---
+++ Collections/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Models
+{
+    internal class Person
+    {
+        public Person(string name, string description, string street, int number ) 
+        { 
+            this.Name = name;
+            this.Description = description;
+            this.Address = new Address(street, number);
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public Address(string street, int number) 
+        { 
+            this.Number = number;
+            this.Street = street;
+        }
+        public int Number { get; set; }
+        public string Street { get; set; }
+        
+    }
+}
