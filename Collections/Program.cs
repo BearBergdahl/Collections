ae943e40a731cc40ca34fc3f3f0f255e541c81b6
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-22 12:50:12 +0100

    Added example of generic method and a simple example of a delegate method.

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Collections.Models;
+using System.ComponentModel.Design;
 
 namespace Collections
 {
     internal class Program
     {
+        public delegate void Callback(string message);
+        
         static void Main(string[] args)
         {
             Person Bear = new Person("Björn", "Lärare", "Gäddvägen", 36);
@@ -15,7 +18,7 @@ namespace Collections
             Console.WriteLine(AnyPerson.Name);
             List<Person> list = new List<Person>();
             list.Add(toPlaywith);
-            list.Add(Bear); 
+            list.Add(Bear);
             list.Add(AnyPerson);
             foreach (Person person in list)
             {
@@ -28,6 +31,42 @@ namespace Collections
 
             DemoClass.DemoObjects();
             DemoClass.DemoArrayList();
+            List<string> stringList = new List<string> { "Hej", "Världen" };
+            PrintTypeOfElement(stringList);
+            List<ScoreCard> scoreList = new List<ScoreCard> { new ScoreCard { PersonId = Guid.NewGuid(), CompetitionId = 1, Score = 2 }, new ScoreCard { PersonId = Guid.NewGuid(), CompetitionId = 2, Score = 3 } };
+            PrintTypeOfElement(scoreList);
+
+            Callback handler = DelegateMethod;
+            handler("Hej Världen");
+            handler("Hello!");
+            handler = PrintHelloName;
+            handler("Jimmie");
+
+            TestHandler(handler);
         }
+
+        private static void TestHandler(Callback handler)
+        {
+            handler("Complex stuff happens");
+        }
+
+        public static void PrintTypeOfElement<T>(List<T> listOfElement)
+        {
+            foreach (T element in listOfElement)
+            {
+                Console.WriteLine(typeof(T).Name);
+                if (element.GetType().Equals(typeof(ScoreCard)))
+                {
+                    ScoreCard scoreElement = element as ScoreCard;                    
+                }
+            }
+        }
+
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        } 
+
+        public static void PrintHelloName(string message) { Console.WriteLine("Hello "+message);}
     }
 }
